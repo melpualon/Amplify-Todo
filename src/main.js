4b2ffa9c02e7216cd7803228c809682edad2558b
melpualon <mel@xperto.ph>
2022-03-11 15:17:54 +0800

    test new push from github - cd

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router'
 import Amplify from 'aws-amplify'
 import awsExports from './aws-exports'
 import './assets/tailwind.css'
+import './assets/main.css'
 
 Amplify.configure(awsExports)
 
