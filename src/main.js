524dab016036c54a900cabe15a13eca617c69e86
melpualon <mel@xperto.ph>
2022-03-11 15:11:55 +0800

    added tailwind

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import Amplify from 'aws-amplify'
 import awsExports from './aws-exports'
+import './assets/tailwind.css'
 
 Amplify.configure(awsExports)
 
